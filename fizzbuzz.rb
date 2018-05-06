c052c166fb4cd052c65bed7a04fec75f4f54789e
Malcolm Leake <echelon1089@gmail.com>
2018-05-06 22:14:04 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(number)
   if number % 3 == 0
   return "Fizz"
-  elsif % 5 == 0
+  elsif number % 5 == 0
   return "Buzz"
-  elsif % 15 == 0
+  elsif number % 15 == 0
   return "FizzBuzz"
-  else
+  else number == 0
     return "nil"
 end
 end
